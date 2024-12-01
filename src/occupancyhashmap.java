aa5004397826e4d479d58a5a25d18c8dacc99b85
saabarindirie <sxd247@student.bham.ac.uk>
2024-12-01 19:21:44 +0000

    added some changes such as rowsetfactory

--- src/occupancyhashmap.java
+++ src/occupancyhashmap.java
@@ -1,3 +1,7 @@
+import javax.sql.RowSet;
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetFactory;
+import javax.sql.rowset.RowSetProvider;
 import java.util.*;
 import java.sql.*;
 
@@ -14,12 +18,13 @@ public class occupancyhashmap {
 
     public static Map<String, Map<String, Integer>> hash() {
        Map <String,Map<String,Integer>> ocuppancyMap = new HashMap<>();
-       String sql = ""; // this is where we will put our string for the sql
+       String sql = " "; // this is where we will put our string for the sql
 
        try( Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
             PreparedStatement statement = connection.prepareStatement(sql);
             ResultSet resultSet = statement.executeQuery(sql); // add statement to get sql table and add on to here
-       ){
+       )
+       {
            while(true){
                // nested hashmap gets data rom the sh  cloud - which will be our SQL so we just make a hashmap and then calculate it ans use calculations to get what they need to doo
                //outer layer - using each string combination of day of week and hour in 24 hours system as the key
@@ -32,12 +37,16 @@ public class occupancyhashmap {
                //present count as for the inner layer too
                int count = resultSet.getInt("count");
 
-               ocuppancyMap.putIfAbsent(hour, new HashMap<>());
-               Map<String, Integer> presentMap = ocuppancyMap.get(hour);
-               presentMap.putIfAbsent(username, 0);
-               presentMap.put(username, presentMap.get(username)+1);
+              ocuppancyMap.putIfAbsent(hour, new HashMap<>());
+              Map<String, Integer> presentMap = ocuppancyMap.get(hour);
+              presentMap.putIfAbsent(username, 0);
+              presentMap.put(username, presentMap.get(username)+1);
 
                //closing connection after we have gotten what we have collected our data
+               RowSetFactory factory = RowSetProvider.newFactory();  // adding rowset for jdbc
+               CachedRowSet rowSet = factory.createCachedRowSet();
+               rowSet.populate(resultSet);
+
                connection.close();
                statement.close();
                resultSet.close();
