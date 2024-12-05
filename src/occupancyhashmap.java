ee55eea5fc7a8f11230ac08f4d4afa55ab1c9c84
saabarindirie <sxd247@student.bham.ac.uk>
2024-12-05 18:01:34 +0000

    change a few things

--- src/occupancyhashmap.java
+++ src/occupancyhashmap.java
@@ -14,25 +14,27 @@ public class occupancyhashmap {
 
     public static Map<String, Map<String, List<Integer>>> occupantMap() {
        Map <String,Map<String,List<Integer>>> occupancyMap = new HashMap<>();
-       String sql = "SELECT  * FROM occupancy"; // this is where we will put our string for the sql
+       String sql = "SELECT  * FROM occupancy"; // this is where we will put our string for the sql , will change sepending on how database looks like
 
        try( Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
             PreparedStatement statement = connection.prepareStatement(sql);
             ResultSet resultSet = statement.executeQuery(); // add statement to get sql table and add on to here
        )
+
        {
            while(true){
                // nested hashmap gets data rom the sh  cloud - which will be our SQL so we just make a hashmap and then calculate it ans use calculations to get what they need to doo
                //outer layer - using each string combination of day of week and hour in 24 hours system as the key
                // inner layer - each users’ name will be used as keys to identify the presence counts for the particular individual from 8 a.m. to 22 p.m
-               String day = resultSet.getString("day_id");
-               String hour = resultSet.getString("hour_id"); // this will be the hour for the outer layer
-               String dayHour = day + "-" + hour;
-               String username = resultSet.getString("user_id");
+               String day,hour,dayHour,username;
+               day = resultSet.getString("day_id");
+               hour = resultSet.getString("hour_id"); // this will be the hour for the outer layer
+               dayHour = day + "-" + hour;
+               username = resultSet.getString("user_id");
                //present count as for the inner layer too
                int count = resultSet.getInt("count");
 
-              occupancyMap.putIfAbsent(dayHour, new HashMap<>());
+              occupancyMap.putIfAbsent(dayHour, new HashMap<>()); // adds a key pair value to the map if the key is not there
               Map<String, List<Integer>> presentMap = occupancyMap.get(dayHour); // inner loop
               presentMap.putIfAbsent(username,new ArrayList<>());
               presentMap.get(username).add(count);
