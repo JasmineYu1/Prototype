bb0e4ee2a270754b20de7f3589147834d4f2a744
saabarindirie <sxd247@student.bham.ac.uk>
2024-12-08 08:11:12 +0000

    taking away a few notes that i have mad on the code

--- src/occupancyHashmap.java
+++ src/occupancyHashmap.java
@@ -3,8 +3,8 @@ import java.sql.*;
 
 public class occupancyHashmap {
 
-     public static final String USERNAME = "occupants";//"YourUsername";
-     public static final String PASSWORD = "password";//"YourPassword";
+     public static final String USER = "occupants";//"YourUsername";
+     public static final String PASS = "password";//"YourPassword";
      public static final String URL = "jdbc:postgresql://localhost:5432/occupancymapbase"; // name of database goes here
 
 
@@ -12,7 +12,7 @@ public class occupancyHashmap {
        Map <String,Map<String,List<Integer>>> occupancyMap = new HashMap<>();
        String sql = "SELECT  * FROM occupancy"; // this is where we will put our string for the sql , will change sepending on how database looks like
 
-       try( Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+       try( Connection connection = DriverManager.getConnection(URL,USER,PASS);
             PreparedStatement statement = connection.prepareStatement(sql);
             ResultSet resultSet = statement.executeQuery(); // add statement to get sql table and add on to here
        )
@@ -53,17 +53,3 @@ public class occupancyHashmap {
 
 
 }
-
-// 1. change from storing integers of presence counts to storing a list of presence data
-// 2. having day and hour ids separately, then linking day_id and hour_id using '_',
-// naming it as the outer layer of hashmap
-// 3. update this part accordingly:
-//              ocuppancyMap.putIfAbsent(hour, new HashMap<>());
-//              Map<String, Integer> presentMap = ocuppancyMap.get(hour);
-//              presentMap.putIfAbsent(username, 0);
-//              presentMap.put(username, presentMap.get(username)+1);
-//
-//               //closing connection after we have gotten what we have collected our data
-//               RowSetFactory factory = RowSetProvider.newFactory();  // adding rowset for jdbc
-//               CachedRowSet rowSet = factory.createCachedRowSet();
-//               rowSet.populate(resultSet);
