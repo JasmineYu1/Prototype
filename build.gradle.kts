df464e59a6a0c999b7fbf453fe4b22daa3b35eff
Jasmine Yu <YXY367@student.bham.ac.uk>
2024-12-11 11:00:20 +0000

    changed dependencies in build.gradle.kts for adding JDBC driver

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.postgresql:postgresql:42.6.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
